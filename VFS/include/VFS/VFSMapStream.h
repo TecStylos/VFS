bdffb6619e04b96741d049625579ed1b2f2fa2ba
TecStylos <tecstylos@gmail.com>
2021-07-22 17:53:42 +0200

    Renamed most private methods of class MapStream Removed user-defined compare function in class MapStream

--- VFS/include/VFS/VFSMapStream.h
+++ VFS/include/VFS/VFSMapStream.h
@@ -2,6 +2,7 @@
 
 #include "VFSAbstractFileIO.h"
 #include <set>
+#include <functional>
 
 namespace VFS {
 
@@ -26,9 +27,7 @@ namespace VFS {
 		typedef const Buffer ConstBuffer;
 		typedef ConstBuffer ConstKey, ConstVal;
 		enum class Location { Unspecified = 0, Sorted, Unsorted };
-		enum class Type { Unspecified = 0, Key, Value };
-		typedef bool (*Comparator)(ConstKey, ConstKey, uint64_t);
-		static bool DefaultComparator(ConstKey left, ConstKey right, uint64_t size);
+		enum class Type { Unspecified = 0, Key, Value, Elem };
 	public:
 		MapStream(const std::string& path, AbstractFileIORef afio, uint64_t& keySize, uint64_t& valSize);
 		~MapStream();
@@ -40,21 +39,18 @@ namespace VFS {
 		void optimize();
 		float currOptimization() const;
 		void flush();
-	public:
-		uint64_t getKeySize() const;
-		uint64_t getValSize() const;
 	private:
-		uint64_t internalFindSorted(ConstKey key) const;
-		uint64_t internalFindUnsorted(ConstKey key) const;
-		void internalRead(Location location, Type type, uint64_t index, Buffer buff) const;
-		void internalWrite(Location location, Type type, uint64_t index, ConstBuffer buff);
-		uint64_t internalGetInFileOffset(Location location, Type type, uint64_t elemIndex) const;
-		uint64_t internalGetLocationBegin(Location location) const;
-		uint64_t internalGetInElemOffset(Type type) const;
-		uint64_t internalGetTypeSize(Type type) const;
-		void internalErase();
+		uint64_t findSorted(ConstKey key) const;
+		uint64_t findUnsorted(ConstKey key) const;
+		void read(Location location, Type type, uint64_t index, Buffer buff) const;
+		void write(Location location, Type type, uint64_t index, ConstBuffer buff);
+		uint64_t getOffsetInFile(Location location, Type type, uint64_t elemIndex) const;
+		uint64_t getOffsetLocation(Location location) const;
+		uint64_t getOffsetInElem(Type type) const;
+		uint64_t size(Type type) const;
+		void eraseFinal();
 	private:
-		bool internalCompare(ConstKey leftKey, ConstKey rightKey) const;
+		bool compare(ConstKey leftKey, ConstKey rightKey) const;
 	private:
 		Key makeKey() const;
 		Val makeVal() const;
@@ -72,24 +68,9 @@ namespace VFS {
 			uint64_t nUnsorted = 0;
 		} m_header;
 		#pragma pack(pop)
-		Comparator m_comp = DefaultComparator;
 		static constexpr uint64_t UNSORTED_INDEX_BIT = (1ull << (sizeof(uint64_t) * 8 - 1));
 		std::set<uint64_t> m_toErase;
 	};
-
-	bool MapStream::DefaultComparator(ConstKey left, ConstKey right, uint64_t size)
-	{
-		auto l = (const char*)*left;
-		auto r = (const char*)*right;
-
-		for (uint64_t i = 0; i < size; ++i)
-		{
-			if (l[i] != r[i])
-				return l[i] < r[i];
-		}
-
-		return false;
-	}
 
 	MapStream::MapStream(const std::string& path, AbstractFileIORef afio, uint64_t& keySize, uint64_t& valSize)
 		: m_path(path), m_afio(afio)
@@ -97,8 +78,8 @@ namespace VFS {
 		if (m_afio->exists(m_path))
 		{
 			m_afio->read(m_path, &m_header, sizeof(Header), 0);
-			keySize = m_header.keySize;
-			valSize = m_header.valSize;
+			keySize = size(Type::Key);
+			valSize = size(Type::Value);
 		}
 		else
 		{
@@ -120,17 +101,17 @@ namespace VFS {
 		if (find(key) != -1)
 			return; // TODO: Implemenent proper handling if key already exists
 
-		internalWrite(Location::Unsorted, Type::Key, m_header.nUnsorted, key);
-		internalWrite(Location::Unsorted, Type::Value, m_header.nUnsorted, value);
+		write(Location::Unsorted, Type::Key, m_header.nUnsorted, key);
+		write(Location::Unsorted, Type::Value, m_header.nUnsorted, value);
 		++m_header.nUnsorted;
 	}
 
 	uint64_t MapStream::find(ConstKey key) const
 	{
 		uint64_t index = 0;
-		if ((index = internalFindSorted(key)) != -1)
+		if ((index = findSorted(key)) != -1)
 			return index;
-		if ((index = internalFindUnsorted(key)) != -1)
+		if ((index = findUnsorted(key)) != -1)
 			return index;
 
 		return -1;
@@ -138,7 +119,7 @@ namespace VFS {
 
 	void MapStream::getValue(uint64_t index, Val valBuff) const
 	{
-		internalRead(
+		read(
 			(index & UNSORTED_INDEX_BIT) ? Location::Unsorted : Location::Sorted,
 			Type::Value,
 			(index & ~UNSORTED_INDEX_BIT),
@@ -160,6 +141,10 @@ namespace VFS {
 	{
 		flush();
 
+		Buffer buff(m_header.nUnsorted * size(Type::Elem));
+
+		//std::qsort(*buff, m_header.nUnsorted, size(Type::Elem), m_comp); // TODO: Implement own sort algorithm
+
 		// TODO: Implement optimization
 	}
 
@@ -170,21 +155,11 @@ namespace VFS {
 
 	void MapStream::flush()
 	{
-		internalErase();
+		eraseFinal();
 		m_afio->write(m_path, &m_header, sizeof(Header), 0);
 	}
 
-	uint64_t MapStream::getKeySize() const
-	{
-		return m_header.keySize;
-	}
-
-	uint64_t MapStream::getValSize() const
-	{
-		return m_header.valSize;
-	}
-
-	uint64_t MapStream::internalFindSorted(ConstKey key) const
+	uint64_t MapStream::findSorted(ConstKey key) const
 	{
 		uint64_t stepSize = m_header.nSorted - 2;
 		uint64_t index = stepSize;
@@ -194,9 +169,9 @@ namespace VFS {
 		Key temp = makeKey();
 		do
 		{
-			if (internalCompare(temp, key))
+			if (compare(temp, key))
 				index += stepSize;
-			else if (internalCompare(key, temp))
+			else if (compare(key, temp))
 				index -= stepSize;
 			else
 				result = index;
@@ -207,15 +182,15 @@ namespace VFS {
 		return result;
 	}
 
-	uint64_t MapStream::internalFindUnsorted(ConstKey key) const
+	uint64_t MapStream::findUnsorted(ConstKey key) const
 	{
 		uint64_t index = -1;
 		Key temp = makeKey();
 		for (uint64_t i = 0; i < m_header.nUnsorted; ++i)
 		{
-			internalRead(Location::Unsorted, Type::Key, i, temp);
-			if (!internalCompare(temp, key) &&
-				!internalCompare(key, temp))
+			read(Location::Unsorted, Type::Key, i, temp);
+			if (!compare(temp, key) &&
+				!compare(key, temp))
 			{
 				index = i;
 				break;
@@ -225,13 +200,13 @@ namespace VFS {
 		return index | UNSORTED_INDEX_BIT;
 	}
 
-	void MapStream::internalRead(Location location, Type type, uint64_t index, Buffer buff) const
+	void MapStream::read(Location location, Type type, uint64_t index, Buffer buff) const
 	{
 		m_afio->read(
 			m_path,
 			*buff,
-			internalGetTypeSize(type),
-			internalGetInFileOffset(
+			size(type),
+			getOffsetInFile(
 				location,
 				type,
 				index
@@ -239,13 +214,13 @@ namespace VFS {
 		);
 	}
 
-	void MapStream::internalWrite(Location location, Type type, uint64_t index, ConstBuffer buff)
+	void MapStream::write(Location location, Type type, uint64_t index, ConstBuffer buff)
 	{
 		m_afio->write(
 			m_path,
 			*buff,
-			internalGetTypeSize(type),
-			internalGetInFileOffset(
+			size(type),
+			getOffsetInFile(
 				location,
 				type,
 				index
@@ -253,45 +228,46 @@ namespace VFS {
 		);
 	}
 
-	uint64_t MapStream::internalGetInFileOffset(Location location, Type type, uint64_t elemIndex) const
+	uint64_t MapStream::getOffsetInFile(Location location, Type type, uint64_t elemIndex) const
 	{
 		return
-			internalGetLocationBegin(location) +
-			elemIndex * (m_header.keySize + m_header.valSize) +
-			internalGetInElemOffset(type);
+			getOffsetLocation(location) +
+			elemIndex * size(Type::Elem) +
+			getOffsetInElem(type);
 	}
 
-	uint64_t MapStream::internalGetLocationBegin(Location location) const
+	uint64_t MapStream::getOffsetLocation(Location location) const
 	{
 		switch (location)
 		{
 		case Location::Sorted: return sizeof(Header);
-		case Location::Unsorted: return sizeof(Header) + m_header.nSorted * (m_header.keySize + m_header.valSize);
+		case Location::Unsorted: return sizeof(Header) + m_header.nSorted * (size(Type::Key) + size(Type::Value));
 		}
 		return -1;
 	}
 
-	uint64_t MapStream::internalGetInElemOffset(Type type) const
+	uint64_t MapStream::getOffsetInElem(Type type) const
 	{
 		switch (type)
 		{
 		case Type::Key: return 0;
-		case Type::Value: return internalGetTypeSize(Type::Key);
+		case Type::Value: return size(Type::Key);
 		}
 		return -1;
 	}
 
-	uint64_t MapStream::internalGetTypeSize(Type type) const
+	uint64_t MapStream::size(Type type) const
 	{
 		switch (type)
 		{
 		case Type::Key: return m_header.keySize;
 		case Type::Value: return m_header.valSize;
+		case Type::Elem: return m_header.elemSize;
 		}
 		return 0;
 	}
 
-	void MapStream::internalErase()
+	void MapStream::eraseFinal()
 	{
 		uint64_t nErasedSorted = 0;
 		uint64_t nErasedUnsorted = 0;
@@ -308,7 +284,7 @@ namespace VFS {
 			bool isUnsorted = (index & UNSORTED_INDEX_BIT);
 			index = (index & ~UNSORTED_INDEX_BIT);
 
-			uint64_t blockBegin = internalGetInFileOffset(
+			uint64_t blockBegin = getOffsetInFile(
 				isUnsorted ? Location::Unsorted : Location::Sorted,
 				Type::Key,
 				index + 1
@@ -319,7 +295,7 @@ namespace VFS {
 			if (nextIt == m_toErase.end())
 				break;
 
-			uint64_t blockSize = internalGetInFileOffset(
+			uint64_t blockSize = getOffsetInFile(
 				isUnsorted ? Location::Unsorted : Location::Sorted,
 				Type::Key,
 				(*nextIt & ~UNSORTED_INDEX_BIT)
@@ -328,7 +304,7 @@ namespace VFS {
 			uint64_t nRemaining = blockSize;
 			constexpr uint64_t maxBuffSize = 16384;
 			Buffer buffer(std::min(maxBuffSize, nRemaining));
-			uint64_t blockBeginShifted = blockBegin - m_header.elemSize * (nErasedSorted + nErasedUnsorted + 1);
+			uint64_t blockBeginShifted = blockBegin - size(Type::Elem) * (nErasedSorted + nErasedUnsorted + 1);
 			while (nRemaining != 0)
 			{
 				uint64_t nToMove = std::min(maxBuffSize, nRemaining);
@@ -348,18 +324,27 @@ namespace VFS {
 		m_toErase.clear();
 	}
 
-	bool MapStream::internalCompare(ConstKey leftKey, ConstKey rightKey) const
+	bool MapStream::compare(ConstKey leftKey, ConstKey rightKey) const
 	{
-		return m_comp(leftKey, rightKey, m_header.keySize);
+		auto l = (const char*)*leftKey;
+		auto r = (const char*)*rightKey;
+
+		for (uint64_t i = 0; i < size(Type::Key); ++i)
+		{
+			if (l[i] != r[i])
+				return l[i] < r[i];
+		}
+
+		return false;
 	}
 
 	MapStream::Key MapStream::makeKey() const
 	{
-		return Key(m_header.keySize);
+		return Key(size(Type::Key));
 	}
 
 	MapStream::Val MapStream::makeVal() const
 	{
-		return Val(m_header.valSize);
+		return Val(size(Type::Value));
 	}
 }
